090becd04c25736dad9a8fab86b1087603fc0ade
Diyorbek-Yusupov <diyorbekyusupovdev@gmail.com>
2022-12-12 23:29:00 +0500

    subbanner finished

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import styles from "./App.scss";
 import Navbar from "./components/navbar/Navbar";
 import Showcase from "./components/showcase/Showcase";
+import SubBanner from "./components/subBanner/SubBanner";
 
 function App() {
    return (
@@ -9,6 +10,7 @@ function App() {
          <Navbar />
          <div className="container">
             <Showcase />
+            <SubBanner />
          </div>
       </div>
    );
---
+++ src/components/subBanner/SubBanner.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import styles from "./subBanner.module.scss";
+
+export default function SubBanner() {
+   return (
+      <div className={styles.subBanner}>
+         <h3 className={styles.title}>We sell seeds </h3>
+         <p className={styles.text}>
+            that always sprout and gardening supplies which never break
+         </p>
+         <img
+            className={styles.bgLeft}
+            src="./assets/images/subbaner-left-bg.png"
+            alt=""
+         />
+         <img
+            className={styles.bgRight}
+            src="./assets/images/subbanner-right-bg.png"
+            alt=""
+         />
+      </div>
+   );
+}
