c9ad1dc98d9a041999ef2888be29d228eb24ac30
Diyorbek-Yusupov <diyorbekyusupovdev@gmail.com>
2022-12-12 21:36:38 +0500

    navbar finished

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+import styles from "./navbar.module.scss";
+
+console.log(styles);
+
+export default function Navbar() {
+   const [navIsOpen, setNavIsOpen] = useState(false);
+
+   const togleNavbar = () => setNavIsOpen((prevState) => !prevState);
+   return (
+      <>
+         {navIsOpen && (
+            <div className={styles.overlay} onClick={togleNavbar}></div>
+         )}
+         <div className={styles.navbar}>
+            <div className={styles.container}>
+               <a className={styles.logo} href="">
+                  <img src=".\assets\images\logo.svg" alt="" />
+               </a>
+               <ul
+                  className={`${styles.content} ${
+                     navIsOpen ? styles.openNav : ""
+                  }`}
+               >
+                  <ul className={styles.navigations}>
+                     <li className={styles.navigation}>ALL PRODUCTS</li>
+                     <li className={styles.navigation}>ABOUT SEEDRA</li>
+                     <li className={styles.navigation}>OUR BLOG</li>
+                     <li className={styles.navigation}>CONTACTS</li>
+                  </ul>
+                  <ul className={styles.socials}>
+                     <li className={styles.social}>
+                        <img src="./assets/images/icon-facebook.svg" alt="" />
+                     </li>
+                     <li className={styles.social}>
+                        <img src="./assets/images/icon-instagram.svg" alt="" />
+                     </li>
+                  </ul>
+                  <form
+                     onSubmit={(e) => {
+                        e.preventDefault();
+                     }}
+                     action=""
+                     className={styles.form}
+                  >
+                     <button className={styles["form-btn"]}>
+                        <img src="./assets/images/icon-search.svg" alt="" />
+                     </button>
+                     <input
+                        placeholder="Search"
+                        type="text"
+                        className={styles["form-input"]}
+                     />
+                  </form>
+               </ul>
+               <div className={styles.btns}>
+                  <button>
+                     <img src="./assets/images/icon-like.svg" alt="" />
+                  </button>
+                  <button>
+                     <img src="./assets/images/icon-cart.svg" alt="" />
+                  </button>
+                  <button onClick={togleNavbar} className={styles.burgerBtn}>
+                     <img src="./assets/images/icon-burger.svg" alt="" />
+                  </button>
+               </div>
+            </div>
+         </div>
+      </>
+   );
+}
